b1a081386cee3a0dfb190a223e92a9d01fb1bdd3
gaurav459433 <gaurav459433@gmail.com>
2023-03-15 19:32:39 +0530

    Added: Code For Last Name Validation

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -5,21 +5,41 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
-    public static boolean isValidFirstName(String firstName) {
+    // Method For First Name Validation
+    public boolean isValidFirstName(String firstName) {
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher match = pattern.matcher(firstName);
         return match.matches();
     }
 
+    // Method For Last Name Validation
+    public boolean isValidLastName(String lastname) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher(lastname);
+        return match.matches();
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Program Using Regex: ");
+        UserRegistrationProblem user = new UserRegistrationProblem();
         Scanner sc = new Scanner(System.in);
+        // For First Name
         System.out.println("Enter First Name: ");
-        String name = sc.next();
-        if (isValidFirstName(name))
+        String firstName = sc.next();
+        if (user.isValidFirstName(firstName))
             System.out.println("Entered First Name is Valid: ");
         else
             System.out.println("Entered First Name is Invalid: ");
+        System.out.println();
+
+        // For Last Name
+        System.out.println("Enter Last name");
+        String lastName = sc.next();
+        if (user.isValidLastName(lastName))
+            System.out.println("Entered Last Name is Valid: ");
+        else
+            System.out.println("Entered Last Name is Invalid: ");
     }
 }
